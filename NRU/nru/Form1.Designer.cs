5ee7bae48a52bc9afb9ed5132d7dd06c334b12eb
marcelowzd <mhenrique1970@hotmail.com>
2017-12-15 11:39:09 -0200

    Added FATEC's NRU Algorithm
    Algorithm developed at FATEC to simulate Windows's NRU Algorithm

---
+++ NRU/nru/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace NRU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle49 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle50 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle51 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle52 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle53 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle54 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clnPagina = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnReferenciada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnModificada = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.clnPag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnClasse0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnClasse1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnClasse2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clnClasse3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle49.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle49.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle49.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle49.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle49.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle49.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle49.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle49;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clnPagina,
+            this.clnReferenciada,
+            this.clnModificada});
+            dataGridViewCellStyle50.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle50.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle50.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle50.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle50.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle50.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle50.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle50;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle51.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle51.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle51.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle51.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle51.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle51.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle51.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle51;
+            dataGridViewCellStyle52.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle52.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle52;
+            this.dataGridView1.Size = new System.Drawing.Size(345, 253);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // clnPagina
+            // 
+            this.clnPagina.HeaderText = "Página";
+            this.clnPagina.Name = "clnPagina";
+            this.clnPagina.ReadOnly = true;
+            // 
+            // clnReferenciada
+            // 
+            this.clnReferenciada.HeaderText = "Referenciada";
+            this.clnReferenciada.Name = "clnReferenciada";
+            this.clnReferenciada.ReadOnly = true;
+            // 
+            // clnModificada
+            // 
+            this.clnModificada.HeaderText = "Modificada";
+            this.clnModificada.Name = "clnModificada";
+            this.clnModificada.ReadOnly = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle53.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle53.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle53.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle53.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle53.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle53.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle53.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle53;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clnPag,
+            this.clnClasse0,
+            this.clnClasse1,
+            this.clnClasse2,
+            this.clnClasse3});
+            this.dataGridView2.Location = new System.Drawing.Point(388, 29);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            dataGridViewCellStyle54.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle54.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGridView2.RowsDefaultCellStyle = dataGridViewCellStyle54;
+            this.dataGridView2.Size = new System.Drawing.Size(548, 253);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // clnPag
+            // 
+            this.clnPag.HeaderText = "Página";
+            this.clnPag.Name = "clnPag";
+            this.clnPag.ReadOnly = true;
+            // 
+            // clnClasse0
+            // 
+            this.clnClasse0.HeaderText = "Classe 0";
+            this.clnClasse0.Name = "clnClasse0";
+            this.clnClasse0.ReadOnly = true;
+            // 
+            // clnClasse1
+            // 
+            this.clnClasse1.HeaderText = "Classe 1";
+            this.clnClasse1.Name = "clnClasse1";
+            this.clnClasse1.ReadOnly = true;
+            // 
+            // clnClasse2
+            // 
+            this.clnClasse2.HeaderText = "Classe 2";
+            this.clnClasse2.Name = "clnClasse2";
+            this.clnClasse2.ReadOnly = true;
+            // 
+            // clnClasse3
+            // 
+            this.clnClasse3.HeaderText = "Classe 3";
+            this.clnClasse3.Name = "clnClasse3";
+            this.clnClasse3.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 299);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NRU - Algoritmo de paginação de memória";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnPagina;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnReferenciada;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnModificada;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnPag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnClasse0;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnClasse1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnClasse2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clnClasse3;
+    }
+}
+
---
+++ NRU/nru/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NRU
+{
+    public partial class Form1 : Form
+    {
+        int[] iPaginas = new int[10]; // Inteiro que guarda número da página (P/ tabela apenas)
+        int[] iRef = new int[10];     // Inteiro que guarda a referência (0 ou 1)
+        int[] iMod = new int[10];     // Inteiro que guarda a modificação (0 ou 1)
+
+        int[] iClass = new int[10];   // Guarda a classe da página (0 a 3)
+        int[] iClassDiv = new int[4]; // Contador de cada classe
+
+        int giCounter = 1; // Inteiro que modifica o número da próxima página;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Quando o form carrega
+        {
+            Random iRand = new Random();
+
+            for (int i = 0; i < iPaginas.Length; i++) // Faz até 10 páginas aleatoriamente
+            {
+                iPaginas[i] = i + 1; // Número da página
+                iRef[i] = iRand.Next(0, 2); // Número aleatório de 0 a 1
+                iMod[i] = iRand.Next(0, 2); // Same
+
+                if (iRef[i] == 0 && iMod[i] == 0) // Cadeia de IFs para qualificar cada página
+                {
+                    iClass[i] = 0; // Classifica a classe
+                    dataGridView2.Rows.Add(iPaginas[i], "Sim", "Não", "Não", "Não"); // Tabela
+                    iClassDiv[0]++; // Adiciona um ao contador da classe 0
+                }
+                else if (iRef[i] == 0 && iMod[i] == 1)
+                {
+                    iClass[i] = 1; // Classifica a classe
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Sim", "Não", "Não");
+                    iClassDiv[1]++; // Adiciona um ao contador da classe 1
+                }
+                else if (iRef[i] == 1 && iMod[i] == 0)
+                {
+                    iClass[i] = 2; // Classifica a classe
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Não", "Sim", "Não");
+                    iClassDiv[2]++; // Adiciona um ao contador da classe 2
+                }
+                else if (iRef[i] == 1 && iMod[i] == 1)
+                {
+                    iClass[i] = 3; // Classifica a classe
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Não", "Não", "Sim");
+                    iClassDiv[3]++; // Adiciona um ao contador da classe 3
+                }
+
+                dataGridView1.Rows.Add(iPaginas[i], iRef[i], iMod[i]);
+            }
+        }
+
+        private void RefreshList() // Atualiza a tabela de acordo com as novas informações
+        {
+            for (int i = 0; i < iClassDiv.Length; i++)
+                iClassDiv[i] = 0;
+
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+
+            for (int i = 0; i < iPaginas.Length; i++)
+            {
+                if (iRef[i] == 0 && iMod[i] == 0)
+                {
+                    iClass[i] = 0;
+                    dataGridView2.Rows.Add(iPaginas[i], "Sim", "Não", "Não", "Não");
+                    iClassDiv[0]++;
+                }
+                else if (iRef[i] == 0 && iMod[i] == 1)
+                {
+                    iClass[i] = 1;
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Sim", "Não", "Não");
+                    iClassDiv[1]++;
+                }
+                else if (iRef[i] == 1 && iMod[i] == 0)
+                {
+                    iClass[i] = 2;
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Não", "Sim", "Não");
+                    iClassDiv[2]++;
+                }
+                else if (iRef[i] == 1 && iMod[i] == 1)
+                {
+                    iClass[i] = 3;
+                    dataGridView2.Rows.Add(iPaginas[i], "Não", "Não", "Não", "Sim");
+                    iClassDiv[3]++;
+                }
+
+                dataGridView1.Rows.Add(iPaginas[i], iRef[i], iMod[i]);
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e) // Clicar no FORM troca página
+        {
+            if (iClassDiv[0] > 0) // Se existir a classe 0
+            {
+                int[] IDs = new int[iClassDiv[0]]; int iCounter = 0; // Salva todos os IDs da classe 0
+
+                for (int i = 0; i < iPaginas.Length; i++)
+                    if (iClass[i] == 0)
+                        IDs[iCounter++] = i; // Adiciona os IDs aqui
+
+                Random iRandom = new Random();
+
+                int iNum = iRandom.Next(0, IDs.Length); // Número aleatório entre os IDs
+
+                MessageBox.Show("Trocando página: " + iPaginas[IDs[iNum]], "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                iPaginas[IDs[iNum]] = 10 + giCounter++; // Adiciona uma nova página
+                iRef[IDs[iNum]] = 0; // Referência é 0 pois acabou de chegar
+                iMod[IDs[iNum]] = 0; // Modificador é 0 pois acabou de chegar
+
+                RefreshList();
+            }
+            else if (iClassDiv[1] > 0)
+            {
+                int[] IDs = new int[iClassDiv[1]]; int iCounter = 0;
+
+                for (int i = 0; i < iPaginas.Length; i++)
+                    if (iClass[i] == 1)
+                        IDs[iCounter++] = i;
+
+                Random iRandom = new Random();
+
+                int iNum = iRandom.Next(0, IDs.Length);
+
+                MessageBox.Show("Trocando página: " + iPaginas[IDs[iNum]], "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                iPaginas[IDs[iNum]] = 10 + giCounter++;
+                iRef[IDs[iNum]] = 0;
+                iMod[IDs[iNum]] = 0;
+
+                RefreshList();
+            }
+            else if (iClassDiv[2] > 0)
+            {
+                int[] IDs = new int[iClassDiv[2]]; int iCounter = 0;
+
+                for (int i = 0; i < iPaginas.Length; i++)
+                    if (iClass[i] == 2)
+                        IDs[iCounter++] = i;
+
+                Random iRandom = new Random();
+
+                int iNum = iRandom.Next(0, IDs.Length);
+
+                MessageBox.Show("Trocando página: " + iPaginas[IDs[iNum]], "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                iPaginas[IDs[iNum]] = 10 + giCounter++;
+                iRef[IDs[iNum]] = 0;
+                iMod[IDs[iNum]] = 0;
+
+                RefreshList();
+            }
+            else if (iClassDiv[3] > 0)
+            {
+                int[] IDs = new int[iClassDiv[3]]; int iCounter = 0;
+
+                for (int i = 0; i < iPaginas.Length; i++)
+                    if (iClass[i] == 3)
+                        IDs[iCounter++] = i;
+
+                Random iRandom = new Random();
+
+                int iNum = iRandom.Next(0, IDs.Length);
+
+                MessageBox.Show("Trocando página: " + iPaginas[IDs[iNum]], "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                iPaginas[IDs[iNum]] = 10 + giCounter++;
+                iRef[IDs[iNum]] = 0;
+                iMod[IDs[iNum]] = 0;
+
+                RefreshList();
+            }
+        }
+    }
+}
